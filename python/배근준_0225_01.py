dbd86d31ef1344ac043c471464c00e1393bdfdfa
CrazyThinker <bgjun0204@naver.com>
2026-02-26 08:36:24 +0900

    Homework

--- python/배근준_0225_01.py
+++ python/배근준_0225_01.py
@@ -5,4 +5,4 @@ print(" ** " + num + "단 **")
 num = int(num)
 
 for i in range(1, 10):
-	print("%d * %d = %d" % (num, i, num * i))
+	print("%d * %d = %2d" % (num, i, num * i))
--- python/배근준_0225_02.py
+++ python/배근준_0225_02.py
@@ -7,5 +7,5 @@ if num1 > num2:
 for i in range(num1, num2 + 1):
 	print(" ** %d단 **" % i)
 	for j in range(1, 10):
-		print("%d * %d = %d" % (i, j, i * j))
+		print("%d * %d = %2d" % (i, j, i * j))
 	print("")
