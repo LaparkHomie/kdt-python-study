28486d0e2bb1e7cc916823ea38e10cb56cf7b67e
CrazyThinker <bgjun0204@naver.com>
2026-03-05 08:59:48 +0900

    Homework

---
+++ python/product_csv_manager.py
@@ -0,0 +1,145 @@
+﻿import csv
+import os
+
+keys = "code", "name", "count", "price", "value"
+szInput = "제품코드", "제품명", "수량", "단가", "판매금액"
+szPtr = "%4s", "  %4s", "%4d", " %4d", "%6d"
+title = "입력", "출력", "조회", "수정", "삭제", "종료"
+
+def menu_title():
+	print("*** 제품관리 ***")
+	for i in range(len(title)):
+		print("%d. 제품정보 %s" % (i + 1, title[i]))
+	print()
+
+def input_data():
+	file_first = True
+	if os.path.exists("product_data.csv"):
+		file_first = False
+	fp = open("product_data.csv", "at", encoding="utf-8", newline='')
+	wr = csv.DictWriter(fp, fieldnames=keys)
+
+	rec = []
+	for i in range(0, 4):
+		rec.append(input(szInput[i] + " 입력 => "))
+	rec[2:4] = map(int, rec[2:4])
+	rec.append(rec[2] * rec[3])
+	dct = dict(zip(keys, rec))
+
+	if file_first:
+		wr.writeheader()
+	wr.writerow(dct)
+
+	fp.close()
+
+def print_data():
+	if os.path.exists("product_data.csv"):
+		fp = open("product_data.csv", "r", encoding="utf-8", newline='')
+		lines = csv.DictReader(fp)
+
+		lst = []
+		for dct in lines:
+			dct["count"] = int(dct["count"])
+			dct["price"] = int(dct["price"])
+			dct["value"] = int(dct["value"])
+			lst.append(dct)
+		print("\t" * 3 + "*** 제품관리 ***")
+		print("=" * 40)
+		print("    ".join(szInput))
+		print("=" * 40)
+		total = 0
+		for dct in lst:
+			print("   ".join(szPtr) % tuple(dct.values()))
+			total += dct["value"]
+		print("=" * 40)
+		print("\t" * 3 + "총 판매금액 = %7d" % total)
+
+def search_data():
+	# 제품코드를 입력받아 해당 제품 정보를 출력한다.
+	if os.path.exists("product_data.csv"):
+		product_code = input("조회할 제품코드를 입력 => ")
+
+		fp = open("product_data.csv", "r", encoding="utf-8", newline='')
+		lines = csv.DictReader(fp)
+		# lst = list(csv.DictReader(fp))
+
+		for dct in lines:
+			if dct["code"] == product_code:
+				dct["count"] = int(dct["count"])
+				dct["price"] = int(dct["price"])
+				dct["value"] = int(dct["value"])
+
+				print("\t" * 3 + "*** 제품관리 ***")
+				print("=" * 40)
+				print("    ".join(szInput))
+				print("=" * 40)
+				print("   ".join(szPtr) % tuple(dct.values()))
+				print("=" * 40)
+				break
+		else:
+			print("조회할 제품 코드가 없음")
+
+def update_data():
+	# 제품코드를 입력받아 일치하는 데이터를 찾아서 수량과 단가를 입력받아 금액을 수정 후 파일 전체를 재저장
+	if os.path.exists("product_data.csv"):
+		product_code = input("수정할 제품코드를 입력 => ")
+		fp = open("product_data.csv", "r", encoding="utf-8", newline='')
+		lst = list(csv.DictReader(fp))
+		fp.close()
+
+		for dct in lst:
+			if dct["code"] == product_code:
+				for i in range(2, 4):
+					dct[keys[i]] = int(input(szInput[i] + " 입력 -> "))
+				dct["value"] = dct["count"] * dct["price"]
+				break
+		else:
+			print("수정할 제품 정보가 없음")
+			return
+		fp = open("product_data.csv", "w", encoding="utf-8", newline='')
+		wr = csv.DictWriter(fp, fieldnames=keys)
+		wr.writeheader()
+		wr.writerows(lst)
+		fp.close()
+
+def delete_data():
+	if os.path.exists("product_data.csv"):
+		product_code = input("삭제할 제품코드를 입력 => ")
+		fp = open("product_data.csv", "r", encoding="utf-8", newline='')
+		lst = list(csv.DictReader(fp))
+		fp.close()
+
+		for dct in lst:
+			if dct["code"] == product_code:
+				lst.remove(dct)
+				break
+		else:
+			print("삭제할 제품 정보가 없음")
+			return
+		fp = open("product_data.csv", "w", encoding="utf-8", newline='')
+		wr = csv.DictWriter(fp, fieldnames=keys)
+		wr.writeheader()
+		wr.writerows(lst)
+		fp.close()
+
+if __name__ == "__main__":
+	while True:
+		menu_title()
+		menu = int(input("메뉴를 선택하세요(1~6) => "))
+		if menu == 1:
+			input_data()
+		elif menu == 2:
+			print_data()
+		elif menu == 3:
+			search_data()
+		elif menu == 4:
+			update_data()
+		elif menu == 5:
+			delete_data()
+		elif menu == 6:
+			print("\n프로그램 종료!!\n")
+			break
+		else:
+			print("\n메뉴를 다시 입력하세요!!\n")
+
+		print()
---
+++ python/배근준_0304.py
@@ -0,0 +1,157 @@
+﻿import csv
+import os
+
+DATA_PATH = "sungjuk_data.csv"
+sz_title = "입력", "출력", "조회", "수정", "삭제", "종료"
+keys = "학번", "이름", "국어", "영어", "수학", "총점", "평균", "등급"
+rank_grade = dict(zip(tuple("수우미양가"), (90, 80, 70, 60, 0)))
+sz_ptr = "%-8s", "%-8s", "%-8d", "%-8d", "%-8d", "%-8d", "%-8.2f", "%-8s"
+loop_ptr = 65
+
+def menu_title():
+	print("*** 제품관리 ***")
+	for i in range(len(sz_title) - 1):
+		print("%d. 성적정보 %s" % (i + 1, sz_title[i]))
+	print("%d. 프로그램 %s" % (6, sz_title[5]))
+	print()
+
+def input_sungjuk():
+	first_file = True
+	if os.path.exists(DATA_PATH):
+		first_file = False
+
+	dct = {}
+	for i in range(0, 5):
+		dct[keys[i]] = input(keys[i] + " 입력 => ")
+
+	dct.update(dict(zip(keys[2:5], map(int, map(dct.get, keys[2:5]))))) # 국영수
+	dct["총점"] = sum(map(dct.get, keys[2:5]))                           # 총점
+	dct["평균"] = dct["총점"] / 3                                         # 평균
+	for grade, score in rank_grade.items():
+		if dct["평균"] >= score:
+			dct["등급"] = grade       # 등급
+			break
+
+	fp = open(DATA_PATH, "a", encoding="utf-8", newline='')
+	wr = csv.DictWriter(fp, fieldnames=keys)
+
+	if first_file:
+		wr.writeheader()
+	wr.writerow(dct)
+
+	fp.close()
+
+def print_sungjuk():
+	if os.path.exists(DATA_PATH):
+		fp = open(DATA_PATH, "r", encoding="utf-8", newline='')
+		lines = list(csv.DictReader(fp))
+		fp.close()
+
+		count = len(lines)
+		total = 0
+
+		print(" " * 20 + "*** 성적표 ***")
+		print("=" * loop_ptr)
+		print(*keys, sep="     ")
+		print("=" * loop_ptr)
+		for dct in lines:
+			dct.update(dict(zip(keys[2:6], map(int, map(dct.get, keys[2:6])))))
+			dct["평균"] = float(dct["평균"])
+			print("".join(sz_ptr) % tuple(dct.values()))
+			total += dct["평균"]
+		print("=" * loop_ptr)
+		print("학생수: %d, 전체 평균: %.2f" % (count, total / count))
+	print()
+
+def search_sungjuk():
+	if os.path.exists(DATA_PATH):
+		code = input("조회할 학번을 입력 => ")
+
+		fp = open(DATA_PATH, "r", encoding="utf-8", newline='')
+		lines = list(csv.DictReader(fp))
+		fp.close()
+
+		print(" " * 20 + "*** 성적표 ***")
+		print("=" * loop_ptr)
+		print(*keys, sep="     ")
+		print("=" * loop_ptr)
+		for dct in lines:
+			if dct["학번"] == code:
+				dct.update(dict(zip(keys[2:6], map(int, map(dct.get, keys[2:6])))))
+				dct["평균"] = float(dct["평균"])
+				print("".join(sz_ptr) % tuple(dct.values()))
+				break
+		print("=" * loop_ptr)
+	print()
+
+def update_sungjuk():
+	if os.path.exists(DATA_PATH):
+		code = input("수정할 학번을 입력 => ")
+
+		fp = open(DATA_PATH, "r", encoding="utf-8", newline='')
+		lines = list(csv.DictReader(fp))
+		fp.close()
+
+		for dct in lines:
+			if dct["학번"] == code:
+				for i in range(2, 5):
+					dct[keys[i]] = int(input(keys[i] + " 입력 => "))
+
+				dct["총점"] = sum(map(dct.get, keys[2:5])) # 총점
+				dct["평균"] = dct["총점"] / 3               # 평균
+				for grade, score in rank_grade.items():
+					if dct["평균"] >= score:
+						dct["등급"] = grade
+						break
+
+				fp = open(DATA_PATH, "w", encoding="utf-8", newline='')
+				wr = csv.DictWriter(fp, fieldnames=keys)
+				wr.writeheader()
+				wr.writerows(lines)
+				fp.close()
+				break
+		else:
+			print("수정할 학번이 없음")
+	print()
+
+def delete_sungjuk():
+	if os.path.exists(DATA_PATH):
+		code = input("삭제할 학번을 입력 => ")
+
+		fp = open(DATA_PATH, "r", encoding="utf-8", newline='')
+		lines = list(csv.DictReader(fp))
+		fp.close()
+
+		for dct in lines:
+			if dct["학번"] == code:
+				lines.remove(dct)
+
+				fp = open(DATA_PATH, "w", encoding="utf-8", newline='')
+				wr = csv.DictWriter(fp, fieldnames=keys)
+				wr.writeheader()
+				wr.writerows(lines)
+				fp.close()
+				break
+		else:
+			print("삭제할 학번이 없음")
+	print()
+
+if __name__ == "__main__":
+	while True:
+		menu_title()
+		menu = int(input("메뉴를 선택하세요(1~6) => "))
+		if menu == 1:
+			input_sungjuk()
+		elif menu == 2:
+			print_sungjuk()
+		elif menu == 3:
+			search_sungjuk()
+		elif menu == 4:
+			update_sungjuk()
+		elif menu == 5:
+			delete_sungjuk()
+		elif menu == 6:
+			print("\n프로그램 종료!!\n")
+			break
+		else:
+			print("\n메뉴를 다시 입력하세요!!\n")
